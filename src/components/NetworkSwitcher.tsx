b9319420cb2a5955766c3702130031d4f90baa24
hammeddauda <hammed.dauda@doftwerks.com>
2025-01-14 16:19:46 +0100

    setuo project

--- src/components/NetworkSwitcher.tsx
+++ src/components/NetworkSwitcher.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { FC } from 'react'
+import dynamic from "next/dynamic";
 
-const NetworkSwitcher = () => {
+//INTERNAL IMPORT
+import {useNetworkConfiguration} from "../contexts/NetworkConfigurationProvider"
+
+
+const NetworkSwitcher: FC = () => {
+  const { networkConfiguration, setNetworkConfiguration } =useNetworkConfiguration();
+
+
   return (
-    <div>NetworkSwitcher</div>
+    <>
+      <input type="checkbox" id="checkbox" />
+        <label className="switch ">
+        <select
+         value={networkConfiguration}
+         onChange={(e) => setNetworkConfiguration(e.target.value || "devnet")}
+         className="select max-w-xs border-none rounded  bg-default-100/5 outline-0"
+        >
+          <option className=" switch" value="mainnet-beta">mainnet</option>
+          <option className=" switch"  value="devnet">devnet</option>
+          <option className=" switch"  value="testnet">testnet</option>
+        </select>
+        </label>
+      
+    </>
   )
 }
 
-export default NetworkSwitcher
+export default dynamic(()=> Promise.resolve(NetworkSwitcher), {ssr:false});
