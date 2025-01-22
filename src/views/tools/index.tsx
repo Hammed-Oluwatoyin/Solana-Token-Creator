27998bb348b7bcddbabc002f52a73d6b98eb8a84
hammeddauda <hammed.dauda@doftwerks.com>
2025-01-22 13:33:14 +0100

    created the tool UI section

--- src/views/tools/index.tsx
+++ src/views/tools/index.tsx
@@ -1,12 +1,111 @@
 import React from 'react'
+import { MdGeneratingTokens} from "react-icons/md";
+import {IoIosArrowRoundForward} from 'react-icons/io';
+import { LuArrowRightFromLine } from "react-icons/lu"
+
 
 const ToolView = ({  setOpenAirdrop,
   setOpenContact,
   setOpenCreateModal,
   setOpenSendTransaction,
   setOpenTokenMetaData }) => {
+
+  const tools = [
+    {
+      name:"Create Token",
+      icon: <MdGeneratingTokens/>,
+      function: setOpenCreateModal,
+    },
+    {
+      name:"Token Metadata",
+      icon: <MdGeneratingTokens/>,
+      function: setOpenTokenMetaData,
+    },
+    {
+      name:"Contact Us",
+      icon: <MdGeneratingTokens/>,
+      function: setOpenContact,
+    },
+    {
+      name:"Airdrop",
+      icon: <MdGeneratingTokens/>,
+      function: setOpenAirdrop,
+    },
+    {
+      name:"Send Transaction",
+      icon: <MdGeneratingTokens/>,
+      function: setOpenSendTransaction,
+    },
+    {
+      name:"Buddy Transaction",
+      icon: <MdGeneratingTokens/>,
+      function: setOpenSendTransaction,
+    },
+    {
+      name:"Top Tokens ",
+      icon: <MdGeneratingTokens/>,
+      function: setOpenSendTransaction,
+    },
+    {
+      name:"Solana Explorer ",
+      icon: <MdGeneratingTokens/>,
+      function: setOpenSendTransaction,
+    }
+
+
+  ]
+
   return (
-    <div>ToolView</div>
+    <section id="tools" className="py-20">
+          <div className={"container"}>
+              <div className={"mb-10 flex items-end justify-between"}>
+                <div className={"mx-auto max-w-2xl text-center"}>
+                   <h2 className={"mb-4 text-3xl font-medium capitalize text-white"}>
+                     Solana Powerful Tools
+                   </h2>
+                  <p className={"text-default-200 text-sm font-medium"}>
+                    Solana allows users to create and manage tokens using the Solana Program Library (SPL). SPL tokens are versatile and can represent fungible (like currencies) or non-fungible assets (like NFTs).
+                    With its speed, low fees, and developer tools, Solana simplifies token creation and management for various use cases like payments, rewards, and digital collectibles.
+                  </p>
+                </div>
+              </div>
+            <div className={"grid gap-6 sm:grid-cols-2 lg:grid-cols-4 "}>
+              {
+                tools.map((tool, index) => (
+                    <div className={"bg-default-950/40 rounded-xl backdrop-blur-3xl"} onClick={() => tool.function(true)}>
+                      <div className="p-6">
+                        <div className="mb-4 flex items-center gap-4">
+                          <div className={`inline-flex h-10 w-10 items-center justify-center rounded-lg bg-red-500/20 ${index == 0 ? "text-red-500" : index == 1 ? "text-sky-500" : index == 2 ? "text-indigo-500" : index == 3 ? "text-yellow-500" :  "text-teal-500"}`}>
+                            <i data-lucide="dribble" className="">
+                              {tool.icon}
+                            </i>
+                          </div>
+                          <h3 className="text-default-200 text-xl font-medium">
+                            {tool.name}
+                          </h3>
+                        </div>
+                        <a className={"text-primary group relative inline-flex items-center gap-2"}>
+                            <span className="bg-primary/80 absolute -bottom-0 h-px w-7/12 rounded transition-all duration-500 group-hover:w-full"></span>
+                          Select & try  <i data-lucide={"move-right"}>
+                          <LuArrowRightFromLine/>
+                        </i>
+                        </a>
+
+                      </div>
+                    </div>
+                ))
+              }
+            </div>
+            <div className="mt-10 flex justify-center">
+              <a className={"hover:bg-primary-hover bg-primary inline-flex items-center justify-center gap-2 rounded-full px-6 py-2 text-white transition-all duration-500"}>
+                More Tools
+              </a>
+              <i>
+                <IoIosArrowRoundForward/>
+              </i>
+            </div>
+          </div>
+    </section>
   )
 }
 
